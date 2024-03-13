d5dbe1f50d49589a5150492fc5f1efa0451897b3
edsonlima2506 <edsoneduardo526@gmail.com>
2024-03-13 16:25:10 -0300

    :recycle: refactor: Corrige carregamento de folhas de estilo

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -17,10 +17,12 @@
         <link rel="stylesheet" href="{{ asset('css/landing_page.css') }}">
 
         <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <noscript><link rel="stylesheet" href="styles.css"></noscript>
 
         <!-- Line Awesome Icons -->
-        <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
+        <link rel="preload" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+        <noscript><link rel="stylesheet" href="styles.css"></noscript>
     </head>
     <body>
          <!-- Header -->

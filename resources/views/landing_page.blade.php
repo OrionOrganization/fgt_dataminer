563c9edc721db03eb4da78415c138c1560841c2c
edsonlima2506 <edsoneduardo526@gmail.com>
2024-03-14 00:24:27 -0300

    :recycle: refactor: Remove tamanho de imagem

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -151,7 +151,7 @@
             <div class="container text-left">
                 <div class="row" id="history-section">
                   <div class="col">
-                    <img src="" id="history-image" alt="Foto da equipe da FGT" class="w-100 hidden" width="545px" height="365px">
+                    <img src="" id="history-image" alt="Foto da equipe da FGT" class="w-100 hidden">
                     </picture>
                   </div>
                   <div class="col">
